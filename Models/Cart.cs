1cd5c343c6ecc2a26011b89786d42a28524a6ceb
“ST”10143151” <ST10143151@vcconnect.edu.za>
2024-11-12 21:21:36 +0200

    Empty repository but keep commit history

--- Models/Cart.cs
+++
@@ -1,8 +0,0 @@
-
-namespace ABCRetailers.Models;
-public class Cart
-{
-    public int CartId { get; set; }
-    public int UserId { get; set; } // Or a User reference if you have user authentication
-    public List<CartItem> Items { get; set; } = new List<CartItem>();
-}
